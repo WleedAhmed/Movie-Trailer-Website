348f3b0999125bb06a2537bfa674176ec726eb97
Waleed Ahmed <waleed.ali628@yahoo.com>
2018-06-26 13:10:37 +0200

    Edit docstring of class Movie in media.py

--- media.py
+++ media.py
@@ -2,7 +2,14 @@ import webbrowser
 
 
 class Movie:
-    """This class provides a way to store information about some movies"""
+    """class storing information about some movies, 
+    it will be used to create instances for different movies.
+
+	Args:
+        movie_title (str): Movie's title.
+        poster_img (str): url of the poster image.
+        trailer_url (str): url of the movie's trailer.
+    """
 
     def __init__(self, movie_title, poster_img, trailer_url):
         self.title = movie_title
